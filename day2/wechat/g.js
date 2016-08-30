172e01cc98d2b69f055b9289f7bc2fab9ae91aca
xypisces <953945582@qq.com>
2016-08-30 23:19:59 +0800

    update

--- day2/wechat/g.js
+++ day2/wechat/g.js
@@ -2,81 +2,14 @@
 
 
 var sha1 = require('sha1')
-var Promise = require('bluebird')
-var request = Promise.promisify(require('request'))
-
-var prefix = 'https://api.weixin.qq.com/cgi-bin/'
-var api = {
-	accessToken: prefix + 'token?grant_type=client_credential'
-}
-
-function Wechat(opts){
-	var that = this
-	this.appID = opts.appID
-	this.appSecret = opts.appSecret
-	this.getAccessToken = opts.getAccessToken
-	this.saveAccessToken = opts.saveAccessToken
-
-	this.getAccessToken()
-		.then(function(data){
-			try{
-				data = JSON.parse(data)
-			}
-			catch(e){
-				return that.updateAccessToken()
-			}
-			if(that.isVaildAccessToken(data)){
-				Promise.resolve(data)
-			}else{
-				return that.updateAccessToken()
-			}
-		})
-		.then(function(data){
-			that.access_token = data.access_token
-			that.expires_in = data.expires_in
-			that.saveAccessToken(data)
-		})
-}
-
-Wechat.prototype.isVaildAccessToken = function(data){
-	if( !data || !data.access_token || !data.expires_in){
-		return false
-	}
-	var access_token = data.access_token
-	var expires_in = data.expires_in
-	var now = (new Date().getTime())
-	if(now < expires_in){
-		return true
-	}else{
-		return false
-	}
-}
-
-Wechat.prototype.updateAccessToken = function() {
-	var appID = this.appID
-	var appSecret = this.appSecret
-	var url = api.accessToken + '&appid=' + appID + '&secret='+appSecret
-
-	return new Promise(function(resolve,reject){
-		request({url: url, json: true })
-		 .then( function(response) {
-			var data = response['body']
-			var now = (new Date().getTime())
-			var expires_in = now + ( data.expires_in - 20 ) * 1000
-			data.expires_in = expires_in
-
-			resolve(data)
-		})
-	})
-}
-
+var getRawBody = require('raw-body')
+var Wechat = require('./wechat')
+var util = require('./util')
 
 module.exports = function(opts){
-	var wechat = new Wechat(opts)
+	// var wechat = new Wechat(opts)
 
 	return function *(next){
-		console.log(this.query)
-
 		var token = opts.token
 		var signature = this.query.signature
 		var nonce = this.query.nonce
@@ -85,10 +18,48 @@ module.exports = function(opts){
 		var str = [token, timestamp, nonce].sort().join('')
 		var sha = sha1(str)
 
-		if(sha === signature){
-			this.body = echostr + ''
-		}else{
-			this.body = 'wrong'
+		if(this.method === 'GET'){
+			if(sha === signature){
+				this.body = echostr + ''
+			}else{
+				this.body = 'wrong'
+			}		
+		}
+		else if(this.method === 'POST'){
+			if(sha != signature){
+				this.body = 'wrong'
+				return false
+			}
+
+			var data = yield getRawBody(this.req,{
+				length: this.length,
+				encoding:this.charset,
+				limit: '1mb'
+			})
+
+			var content = yield util.parseXMLAsync(data)
+			console.log(content)
+
+			var message = util.formatMessage(content.xml)
+			console.log(message)
+
+			if(message.MsgType === 'event'){
+				if(message.Event === 'subscribe'){
+					var now = new Date().getTime()
+
+					this.status = 200
+					this.type = 'application/xml'
+					this.body = ' <xml>' +
+						 '<ToUserName><![CDATA['+ message.FromUserName +']]></ToUserName>' +
+						 '<FromUserName><![CDATA['+ message.ToUserName +']]></FromUserName> ' +
+						 '<CreateTime>'+ now +'</CreateTime>' +
+						 '<MsgType><![CDATA[text]]></MsgType>' +
+						 '<Content><![CDATA[你好哇,李银河!]]></Content>' +
+						 '<MsgId>'+ message.MsgId +'</MsgId>' +
+						 '</xml>'
+					return 	 
+				}
+			}
 		}
 	}
 }
